a7058abb95fd9837366a88a0d9c7e19266050343
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2018-03-11 13:15:05 -0400

    split dynamic setup

--- resources/leiningen/new/dynamic_quil/core.clj
+++ resources/leiningen/new/dynamic_quil/core.clj
@@ -1,9 +1,23 @@
 (ns {{name}}.core
   (:require [quil.core :as q])
+  (:require [quil.middleware :as m])
   (:require [{{name}}.dynamic :as dynamic]))
 
-(defn sketch []
-  (println 'hello))
+(defn setup []
+  (q/frame-rate 30)
+  (q/background 255 255 255)
+  ; Return the initial state from the setup function.
+  {:r 0
+   :g 0
+   :b 0})
+
+(q/defsketch sketch
+  :title "My dynamic Quil sketch"
+  :setup setup
+  :draw dynamic/draw
+  :update dynamic/update-state
+  :middleware [m/fun-mode]
+  :size [500 500])
 
 (defn -main []
   sketch)
--- resources/leiningen/new/dynamic_quil/dynamic.clj
+++ resources/leiningen/new/dynamic_quil/dynamic.clj
@@ -1,6 +1,14 @@
-(ns {{name}}.dynamic)
+(ns {{name}}.dynamic
+  (:require [quil.core :as q]))
 
-; (ns {{name}}.dynamic
-;    (:require [quil.core as q])
-;   )
+(defn update-state [state]
+  {:r (Math/min 255 (+ (:r state) 1))
+   :g 0
+   :b 0})
 
+(defn draw [state]
+  (q/fill (:r state) (:g state) (:b state))
+  (q/ellipse 100 100 100 100)
+  
+  ; This line allows us to edit on the fly.
+  (use :reload '{{name}}.dynamic))
