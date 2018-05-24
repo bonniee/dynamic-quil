f9d41e6a55b7b1d921de2e011dc01cbe120ca9cd
Bonnie <bonnie@mokosha.localdomain>
2018-05-24 18:54:57 -0400

    rm snapshot from version, bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dynamic-quil/lein-template "0.1.0-SNAPSHOT"
+(defproject dynamic-quil/lein-template "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
