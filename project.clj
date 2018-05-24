7cde30b86bf8ccd5def1313ba7a6991db4e81a2b
Bonnie Eisenman <bonnie.eisenman@gmail.com>
2018-05-24 18:57:04 -0400

    url and descr

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject dynamic-quil/lein-template "0.2.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A lein template for a repl-friendly quil project"
+  :url "https://github.com/bonniee/dynamic-quil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
